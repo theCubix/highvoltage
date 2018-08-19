0577f3f335a134408caa43454823512316d068b1
[thecubix] <[timon.forrer@gmail.com]>
2018-08-19 23:05:57 +0200

    Test contentful source plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,47 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const IndexPage = () => (
+export default ({data}) => {
+  console.log(data)
+  return (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Videos</h1>
+    {data.allContentfulVideo.edges.map(({ node }, index) => (
+      <div key="index">
+        <p><b>Title:</b> {node.title}</p>
+        <p><b>Contentful ID:</b> {node.contentfulid}</p>
+        <p><b>Slug:</b> {node.slug}</p>
+        <p><b>Description:</b><br /> {node.description.description}</p>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${node.contentfulid}?rel=0&amp;showinfo=0`}
+          frameBorder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          title={index}></iframe>
+      </div>
+    ))}
   </Layout>
-)
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allContentfulVideo {
+      edges {
+        node {
+          contentfulid
+          title
+          slug
+          description {
+            description
+          }
+        }
+      }
+    }
+  }
+  `
+
