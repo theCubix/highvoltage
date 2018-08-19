470292571a866826838add153b67e662aee6d39b
[thecubix] <[timon.forrer@gmail.com]>
2018-08-19 23:05:22 +0200

    Removed unecessary styles

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,6 @@ import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
   <div className="header">
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -21,7 +14,6 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
   </div>
 )
 
