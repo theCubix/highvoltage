d0c1e5266fa11a1352300b0128434f7c7126ebdf
[thecubix] <[timon.forrer@gmail.com]>
2018-08-20 22:20:40 +0200

    Added heading component

---
+++ src/components/heading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Heading = ({title, letter}) => (
+  <div className="heading__wrapper">
+    <h1 className="heading heading--level-1 heading--margin-top-large z-1">{title}</h1>
+    <span className="heading heading__letter">{letter}</span>
+  </div>
+);
+
+export default Heading;
