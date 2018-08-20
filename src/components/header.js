670fb9adfe73e5ff34ac215fc7f8661a6d06c69b
[thecubix] <[timon.forrer@gmail.com]>
2018-08-20 21:07:28 +0200

    Hide header for the moment

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <div className="header">
+  <div className="header" style={{display: 'none'}}>
       <h1 style={{ margin: 0 }}>
         <Link to="/">
           {siteTitle}
