dcbd077c45b105b37f642fbc2d9d900d2af3a323
[thecubix] <[timon.forrer@gmail.com]>
2018-08-20 20:49:38 +0200

    Added a component for the hero image

---
+++ src/components/hero-main.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+import Img from 'gatsby-image'
+
+const HeroImage = ({data}) => (
+  <StaticQuery
+    query={graphql`
+      query Image {
+        MainImage: file(relativePath: {eq: "images/voltage-arc-live-rockarena.jpg" }) {
+          childImageSharp {
+            resolutions(
+              width: 1200,
+              traceSVG: {
+                color: "#c31818",
+                background: "#a31414"
+              }) {
+                ...GatsbyImageSharpResolutions_withWebp_tracedSVG
+            }
+          }
+        }
+      }`
+    }
+    render={data => (
+      <Img
+        style={{
+          width: "100%",
+          height: 0,
+          paddingTop: "41.125%"
+        }}
+        resolutions={data.MainImage.childImageSharp.resolutions}
+      />
+    )}
+    />
+)
+
+export default HeroImage
