f26369254bb4e2ae037c3fd1beadd0a19654468e
[thecubix] <[timon.forrer@gmail.com]>
2018-08-23 23:15:44 +0200

    Added the video detail view component

---
+++ src/components/video-detail.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+class VideoDetail extends React.Component {
+  render() {
+    const {
+      id,
+      title,
+      youTubeId,
+      description
+    } = this.props.video
+
+    return(
+      <div>
+        <p><b>id:&nbsp;</b>{id}</p>
+        <p><b>title:&nbsp;</b>{title}</p>
+        <p><b>youTubeId:&nbsp;</b>{youTubeId}</p>
+        <p><b>description:&nbsp;</b>{description}</p>
+      </div>
+    )
+  }
+}
+
+export default VideoDetail
+
+export const videoDetailFragment = graphql`
+  fragment VideoDetail_details on ContentfulVideo {
+    #specify the fields we want to query
+    id
+    title
+    youTubeId
+    description {
+      description
+    }
+  }
+`
