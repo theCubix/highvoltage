98d80fe7554e99fb0faedf3df05deefe735ecd4d
[thecubix] <[timon.forrer@gmail.com]>
2018-08-26 21:36:55 +0200

    Restructured component

--- src/components/home/videos.js
+++ src/components/home/videos.js
@@ -1,78 +1,42 @@
-import * as PropTypes from 'prop-types'
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
 import Slider from 'react-slick'
 
 import Video from '../../components/video'
 
-let settings = {
-  dots: true,
-  infinite: false,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  speed: 500,
-  responsive: [
-    {
-      breakpoint: 450,
-      settings: {
-        slidesToShow: 1
-      }
-    }
-  ]
-}
 
-const Videos = ({ data }) => (
-  <div className="container container--video">
-  
-    <Slider {...settings}>
-      {data.allContentfulVideo.edges.map((node) => (
-        <Video
-          location={this.props.location}
-          key={node.id}
-          video={node}
-        />
-      ))}
+class Videos extends React.Component {
+  render() {
+    let videos = this.props.videos
 
-    </Slider>
-  </div>
-)
-
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query Videos {
-        allContentfulVideo {
-          edges {
-            node {
-              id
-              title
-              slug
-              thumbnail {
-                fixed(width: 426, height: 240) {
-                  base64
-                  tracedSVG
-                  aspectRatio
-                  width
-                  height
-                  src
-                  srcSet
-                  srcWebp
-                  srcSetWebp
-                }
-              }
-            }
+    //configuration for the slider
+    let settings = {
+      dots: true,
+      infinite: false,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      speed: 500,
+      responsive: [
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToShow: 1
           }
         }
-      }`
+      ]
     }
-    render={data => (<Videos data={data} {...props} />)}
-  />
-)
-
-Videos.propTypes = {
-  data: PropTypes.shape({
-    allContentfulVideo: PropTypes.object
-  })
+    return(
+      <div className="container container--video" style={{ paddingBottom: '32px' }}>
+        <Slider {...settings}> 
+          {videos.map((video) => (
+            <Video
+              key={video.id}
+              video={video}
+            />
+          ))}
+        </Slider>
+      </div>
+    )
+  }
 }
 
-//TODO: Fix this goddamn component
+export default Videos
