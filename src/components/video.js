3ba28922ce35adca890758ed5c7830400a05f737
[thecubix] <[timon.forrer@gmail.com]>
2018-08-26 18:53:10 +0200

    Added real content to video component

--- src/components/video.js
+++ src/components/video.js
@@ -2,17 +2,37 @@ import * as PropTypes from 'prop-types'
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Img from 'gatsby-image'
+
 class Video extends React.Component {
+
   static propTypes = {
     video: PropTypes.shape ({
       slug: PropTypes.string,
-      title: PropTypes.string
+      title: PropTypes.string,
+      thumbnail: PropTypes.object
     }).isRequired
   }
   render() {
-    const { slug, title } = this.props.video
+
+    const video = {
+      width: '100%',
+      paddingTop: '56.25%',
+      backgroundColor: 'red',
+      boxSizing: 'border-box'
+    }
+
+    const { slug, thumbnail } = this.props.video
     return(
-      <Link to={`/video/${slug}/`}>{title}</Link>
+      <Link className="video-thumb" to={`/video/${slug}/`}>
+        <div className="video-thumb__wrapper" >
+          <Img
+            fixed={thumbnail.fixed}
+            outerWrapperClassName="video-thumb__image-wrapper"
+            className="video-thumb__image"
+            />
+        </div>
+      </Link>
     )
   }
 }
