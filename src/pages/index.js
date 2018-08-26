874885b71be60132edc3035478570e52062f4819
[thecubix] <[timon.forrer@gmail.com]>
2018-08-26 21:37:07 +0200

    Use index page to query for videos

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,19 +17,45 @@ class Index extends React.Component {
   }
 
   render() {
+    //Prepare allContentfulVideo for usage in component
+    let { allContentfulVideo } = this.props.data
+    const videos = allContentfulVideo.edges.map(e => e.node)
 
     return (
       <Layout location={this.props.location}>
         <HeroImage />
         <MusicIsOurLife />
-
-        
-
-        <Videos />
-
+        <Videos videos={videos} />
       </Layout>
     )
   }
 }
 
 export default Index
+
+export const pageQuery = graphql`
+  query {
+    allContentfulVideo {
+      edges {
+        node {
+          id
+          title
+          slug
+          thumbnail {
+            fixed(width: 426, height: 240) {
+              base64
+              tracedSVG
+              aspectRatio
+              width
+              height
+              src
+              srcSet
+              srcWebp
+              srcSetWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
