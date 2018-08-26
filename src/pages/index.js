e8c7f7a743a96f67d1f95247b4bc49632d3f5de6
[thecubix] <[timon.forrer@gmail.com]>
2018-08-26 18:54:03 +0200

    Call video component on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import * as PropTypes from 'prop-types'
 import React from 'react'
-import { graphql } from 'gatsby'
+//import { graphql } from 'gatsby'
 
 import HeroImage from '../components/home/hero-main'
 import MusicIsOurLife from '../components/home/music-is-our-live'
+import Videos from '../components/home/videos'
 
-import Video from '../components/video'
 import Layout from '../layouts/main'
 
 class Index extends React.Component {
@@ -17,24 +17,15 @@ class Index extends React.Component {
   }
 
   render() {
-    let { allContentfulVideo } = this.props.data
 
-    const videos = allContentfulVideo.edges.map(e => e.node)
-
-    console.log(videos)
-    
     return (
       <Layout location={this.props.location}>
         <HeroImage />
         <MusicIsOurLife />
 
-        {videos.map((video) => (
-          <Video
-            location={this.props.location}
-            key={video.slug}
-            video={video}
-            />
-        ))}
+        
+
+        <Videos />
 
       </Layout>
     )
@@ -42,16 +33,3 @@ class Index extends React.Component {
 }
 
 export default Index
-
-export const pageQuery = graphql`
-  query {
-    allContentfulVideo {
-      edges {
-        node {
-          slug
-          title
-        }
-      }
-    }
-  }
-`
