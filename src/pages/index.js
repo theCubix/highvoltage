77a673f3d3cb5dc72141e1fed248e53bd3db61e2
[thecubix] <[timon.forrer@gmail.com]>
2018-08-26 16:10:45 +0200

    Added videos to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,57 @@
+import * as PropTypes from 'prop-types'
 import React from 'react'
-//import { graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import HeroImage from '../components/home/hero-main'
 import MusicIsOurLife from '../components/home/music-is-our-live'
 
+import Video from '../components/video'
 import Layout from '../layouts/main'
 
-export default ({data}) => {
-  console.log(data)
-  return (
-    <Layout>
-      <HeroImage />
-      <MusicIsOurLife />
-    </Layout>
-  )
+class Index extends React.Component {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+    data: PropTypes.shape({
+      allContentfulVideo: PropTypes.object,
+    }),
+  }
+
+  render() {
+    let { allContentfulVideo } = this.props.data
+
+    const videos = allContentfulVideo.edges.map(e => e.node)
+
+    console.log(videos)
+    
+    return (
+      <Layout location={this.props.location}>
+        <HeroImage />
+        <MusicIsOurLife />
+
+        {videos.map((video) => (
+          <Video
+            location={this.props.location}
+            key={video.slug}
+            video={video}
+            />
+        ))}
+
+      </Layout>
+    )
+  }
 }
+
+export default Index
+
+export const pageQuery = graphql`
+  query {
+    allContentfulVideo {
+      edges {
+        node {
+          slug
+          title
+        }
+      }
+    }
+  }
+`
