b2325a8b83d20d65c4043e9dc321da4b9a4b243c
[thecubix] <[timon.forrer@gmail.com]>
2018-08-30 19:33:28 +0200

    Added videos page

---
+++ src/pages/videos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../layouts/main'
+
+const VideosPage = () => (
+  <Layout>
+    <main className="container">
+      <h1 className="heading heading--level-2">Videos</h1>
+      <p className="text">Hey!</p>
+    </main>
+  </Layout>
+)
+
+export default VideosPage
