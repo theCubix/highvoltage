caa695ba43dee4acdeb20411e94610a5a7a63b23
[thecubix] <[timon.forrer@gmail.com]>
2018-09-09 22:30:05 +0200

    Small fix (added class of 'undefined' if no className)

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Heading = ({title, letter, className}) => (
-  <div className={`heading__wrapper ${className}`}>
+  <div className={`heading__wrapper ${ className ? className : '' }`}>
     <h1 className="heading heading--level-1 heading--margin-top-large z-1">{title}</h1>
     <span className="heading heading__letter">{letter}</span>
   </div>
