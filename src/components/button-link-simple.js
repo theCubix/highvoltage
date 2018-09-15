42a47b37f15d984f12fd411bd74db6904002f01b
[thecubix] <[timon.forrer@gmail.com]>
2018-09-15 10:13:09 +0200

    Simplified props declaration

--- src/components/button-link-simple.js
+++ src/components/button-link-simple.js
@@ -2,15 +2,17 @@ import React from 'react'
 
 class ButtonLink extends React.Component {
   render() {
-    const dest = this.props.dest;
-    const title = this.props.children;
-    const classNameProp = this.props.className;
+    const {
+      dest,
+      children,
+      classNameProp
+    } = this.props
 
     return (
       <a
         href={dest}
         className={`btn-link-simple text text--line-height-narrower z-1 ${classNameProp}`}
-      >{title}</a>
+      >{children}</a>
     )
   }
 }
