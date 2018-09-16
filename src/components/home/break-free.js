4b52b85505bf9c72aa3db3d00059d9917e85386c
[thecubix] <[timon.forrer@gmail.com]>
2018-09-16 20:30:34 +0200

    Now uses styled components

--- src/components/home/break-free.js
+++ src/components/home/break-free.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import styled from 'react-emotion'
 
-import Heading from '../heading'
-import ButtonLink from '../button-link-simple'
+import { MediaQueries as media, RoundedCorners as rounded } from '../../style-variables'
+import Img from 'gatsby-image'
+import ButtonLink from '../ButtonLink'
 import Container from '../Container'
+import { SectionTitle as Heading, SubTitle } from '../Typography'
 
 const links = [
     { id: 1, title: 'Apple Music', url: 'https://itunes.apple.com/ch/album/break-free-ep/1372030544?app=music' },
@@ -12,6 +14,33 @@ const links = [
     { id: 3, title: 'Google Music', url: 'https://play.google.com/music/m/Bxsjbp5xywdgjbjtz6qnixvcixu?t=Break_Free_-_Voltage_Arc' },
     { id: 4, title: 'YouTube', url: 'https://www.youtube.com/playlist?list=PLM1AaulkmqOJJ9Csx5OSrjq3sQ2GVBJub' }
 ]
+
+const Grid = styled('div')`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 300px auto;
+  grid-template-rows: 300px;
+  ${media.mobile} {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+  }
+`
+
+const Cover = styled(Img)`
+  ${rounded};
+  ${media.mobile} {
+    display: none;
+  }
+`
+
+const Playlist = styled('iframe')`
+  background: transparent;
+  overflow: hidden;
+  width: 100%;
+  ${media.mobile} {
+    height: 480px;
+  }
+`
 
 const MusicIsOurLife = ({ siteTitle }) => (
   <StaticQuery
@@ -28,43 +57,38 @@ const MusicIsOurLife = ({ siteTitle }) => (
     `}
     render={data => 
       <>
-        <Heading className="container" title="Break Free – unsere erste EP" letter="B" />
+        <Container>
+          <Heading>Break Free &ndash; unsere erste EP</Heading>
+        </Container>
 
         <Container video marginBottom="narrow">
-          
-          <div className="grid break-free-grid margin-bottom--narrower">
 
-            <Img className="break-free-cover rounded-corners" fluid={data.cover.childImageSharp.fluid} />
+          <Grid>
 
-            <iframe
-              className="break-free-embed"
+            <Cover fluid={data.cover.childImageSharp.fluid} />
+
+            <Playlist
               title="Break Free EP"
               allow="autoplay *; encrypted-media *;"
               frameBorder="0"
               height="300"
-              style={{
-                width: '100%',
-                overflow: 'hidden',
-                background: 'transparent'
-              }}
               sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
-              src="https://tools.applemusic.com/embed/v1/album/1372030544?country=ch"
-            ></iframe>
+              src="https://tools.applemusic.com/embed/v1/album/1372030544?country=ch"></Playlist>
 
-          </div>
+          </Grid>
 
-       
        </Container>
 
        <Container marginBottom="default">
-        
 
-          <h2 className="heading heading--level-2 heading--nomargin-top">Erhältlich bei</h2>
+          <SubTitle>Erhältlich bei</SubTitle>
 
           {links.map(link => (
             <ButtonLink
               key={link.id}
-              dest={link.url}>
+              dest={link.url}
+              simple
+              outbound>
               {link.title}
             </ButtonLink>
           ))}
