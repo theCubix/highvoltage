5cd33c18bc448b0c544a66a1296a8960aca928ca
[thecubix] <[timon.forrer@gmail.com]>
2018-10-16 23:51:47 +0200

    Updated overview page

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -1,13 +1,57 @@
 import React from 'react'
 import styled from 'react-emotion'
 import Layout from '../layouts/main'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
+import Img from 'gatsby-image'
+
+import { MediaQueries as media, Colours as colours, RoundedCorners as rounded } from '../style-variables'
 
 import Container from '../components/Container'
-import { SubTitle as Title } from '../components/Typography'
-import ButtonLink from '../components/ButtonLink'
+import { SubTitle as Title, TextBase } from '../components/Typography'
 
-const Wrapper = styled('div')``
+const VideoItem = styled('div')``
+
+const ThumbnailWrapper = styled(Link)`
+  display: block;
+  padding-top: 60px;
+  width: 100%;
+  ${media.mobile} {
+    padding-top: 40px;
+  }
+`
+
+const Thumbnail = styled(Img)`
+  ${rounded};
+  /* width: 100%; */
+  transition: opacity 300ms ease;
+  ${VideoItem}:hover & {
+    opacity: 0.8;
+  }
+`
+
+const ContentWrapper = styled(Link)`
+  ${TextBase};
+  color: ${colours.basePrimary};
+  display: block;
+  padding-top: 20px;
+  padding-left: 16px;
+  padding-right: 16px;
+  text-decoration: none;
+`
+
+const VideoTitle = styled('h2')`
+  line-height: 1.8em;
+  font-size: 1.8em;
+  font-weight: 400;
+  margin: 0;
+  ${VideoItem}:hover & {
+    text-decoration: underline;
+  }
+  ${media.mobile} {
+    font-size: 1.1em;
+    line-height: 1.3em;
+}
+`
 
 const VideosPage = ({ data }) => (
   <Layout>
@@ -16,9 +60,15 @@ const VideosPage = ({ data }) => (
       <Title>Videos</Title>
       
       {data.allContentfulVideo.edges.map(({ node }, index) => (
-        <Wrapper key={index}>
-          <ButtonLink dest={`/videos/${node.slug}`} simple>{node.title}</ButtonLink>
-        </Wrapper>
+        <VideoItem id={node.slug}>
+          <ThumbnailWrapper key={index} to={`/videos/${node.slug}`}>
+            <Thumbnail fluid={node.thumbnail.fluid} />
+          </ThumbnailWrapper>
+
+          <ContentWrapper>
+            <VideoTitle>{node.title}</VideoTitle>
+          </ContentWrapper>
+        </VideoItem>
       ))}
 
     </Container>
