5ddd1832c986ccef7170d6b247d6f1c0d1f40aee
[thecubix] <[timon.forrer@gmail.com]>
2018-10-16 23:51:59 +0200

    Actual structured date

--- src/templates/video-page.js
+++ src/templates/video-page.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import VideoDetail from '../components/video-detail'
 import Layout from '../layouts/main'
+import Helmet from 'react-helmet'
 
 class VideoTemplate extends React.Component {
   static propTypes = {
@@ -13,6 +14,29 @@ class VideoTemplate extends React.Component {
   render() {
     return(
       <Layout>
+        <Helmet
+          script={[{
+            "type": "application/ld+json",
+            "innerHTML": `{
+              "@context": "http://schema.org",
+              "@type": "VideoObject",
+              "name": "${this.props.data.contentfulVideo.title}",
+              "description": "${this.props.data.contentfulVideo.description.childMarkdownRemark.excerpt}",
+              "thumbnailUrl": "https:${this.props.data.contentfulVideo.thumbnail.fixed.src}",
+              "uploadDate": "${this.props.data.contentfulVideo.createdAt}",
+              "publisher": {
+                "@type": "Organization",
+                "name": "Voltage Arc",
+                "logo": {
+                  "@type": "ImageObject",
+                  "url": "https://images.ctfassets.net/7dnxtb8wlc24/1iD3jSdBiuaW6YGkAKksiW/c0222acd2f7f176ef6b2624e00f5e28c/shortcut-chrom-schwarz.jpeg?w=100&q=100&fm=jpg",
+                  "width": 100,
+                  "height": 100
+                }
+              },
+              "contentUrl": "https://www.youtube.com/embed/${youTubeId}"
+            }`
+          }]} />
         <VideoDetail video={this.props.data.contentfulVideo} />
       </Layout>
     )
