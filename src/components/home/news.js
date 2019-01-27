a874e573670b3cd8af34b650dfacf8d624b1b7e5
[thecubix] <[timon.forrer@gmail.com]>
2019-01-27 18:30:54 +0100

    created reusable component and removed code

--- src/components/home/news.js
+++ src/components/home/news.js
@@ -1,113 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import styled from 'react-emotion'
-import Img from 'gatsby-image'
 
+import ButtonLink from '../ButtonLink'
 import Container from '../Container'
-import { SectionTitle as Title, TextBase, Text } from '../Typography'
-import { RoundedCorners as rounded, Colours as colours, MediaQueries as media } from '../../style-variables'
-
+import { NewsHeroPreview, NewsPreview } from '../NewsPreview'
+import { SectionTitle as Title } from '../Typography'
 
 class News extends React.Component {
   render() {
     const entries = this.props.entries
     const heroEntry = entries[0]
-
-    const HeroImage = styled(Img)`
-      ${media.tablet} {
-        ${rounded};
-      }
-      ${media.desktop} {
-        ${rounded};
-      }
-      & > div {
-        padding-bottom: 56.25%!important;
-      }
-    `
-
-    const HeroWrapper = styled(Link)`
-      ${TextBase};
-      color: ${colours.basePrimary};
-      display: block;
-      margin: 40px;
-      text-decoration: none;
-      ${media.mobile} {
-        margin: 30px 16px 80px 16px;
-      }
-    `
-    const HeroDate = styled('p')`
-      font-size: 1.25em;
-      margin: 0;
-      opacity: 0.6;
-      ${media.mobile} {
-        font-size: 1.1em;
-        margin-bottom: 10px;
-      }
-    `
-
-    const HeroTitle = styled('h2')`
-      line-height: 2em;
-      font-size: 2.125em;
-      font-weight: 400;
-      margin: 0;
-      ${HeroWrapper}:hover & {
-        text-decoration: underline;
-      }
-      ${media.mobile} {
-        font-size: 1.6em;
-        line-height: 1.4em;
-      }
-    `
-
-    const EntryWrapper = styled('div')`
-      display: flex;
-      margin-bottom: 40px;
-      ${media.mobile} {
-        display: block;
-      }
-    `
-
-    const EntryImage = styled(Img)`
-      ${rounded};
-      height: 200px;
-      width: 200px;
-      ${media.mobile} {
-        height: 160px;
-        margin-left: auto;
-        margin-right: auto;
-        position: relative;
-        width: 100%;
-      }
-    `
-
-    const EntryDate = styled('p')`
-      font-size: 1.1em;
-      margin: 0;
-      opacity: 0.6;
-      ${media.mobile} {
-        margin-bottom: 10px;
-      }
-    `
-
-    const EntryContent = styled(Link)`
-      ${TextBase};
-      color: ${colours.basePrimary};
-      display: block;
-      padding: 0 30px;
-      text-decoration: none;
-      ${media.mobile} {
-        padding: 30px 15px;
-      }
-    `
-
-    const EntryTitle = styled('h2')`
-      font-size: 1.5em;
-      font-weight: 400;
-      margin: 5px 0 10px 0;
-      ${EntryContent}:hover & {
-        text-decoration: underline;
-      }
-    `
 
     return(
       <>
@@ -116,29 +17,27 @@ class News extends React.Component {
         </Container>
 
         <Container video marginBottom="narrower">
-          <Link to={heroEntry.slug}><HeroImage fluid={heroEntry.cover.fluid} /></Link>
-          <HeroWrapper to={heroEntry.slug}>
-            <HeroDate>Veröffentlicht am {heroEntry.createdAt}</HeroDate>
-            <HeroTitle>{heroEntry.title}</HeroTitle>
-            <Text>
-              {heroEntry.body.childMarkdownRemark.excerpt}
-            </Text>
-          </HeroWrapper>
+          <NewsHeroPreview
+            slug={heroEntry.slug}
+            cover={heroEntry.cover}
+            createdAt={heroEntry.createdAt}
+            title={heroEntry.title}
+            excerpt={heroEntry.body} />
         </Container>
 
         <Container>
-          {entries.slice(1).map((entry, index) => (
-            <EntryWrapper key={index}>
-              <Link to={entry.slug}><EntryImage fluid={entry.cover.fluid} /></Link>
-              <EntryContent to={entry.slug}>
-                <EntryDate>{entry.createdAt}</EntryDate>
-                <EntryTitle>{entry.title}</EntryTitle>
-                <Text>
-                  {entry.body.childMarkdownRemark.excerpt}
-                </Text>
-              </EntryContent>
-            </EntryWrapper>
+          {entries.slice(1).map(entry => (
+            <NewsPreview
+              key={entry.slug}
+              slug={entry.slug}
+              cover={entry.cover}
+              createdAt={entry.createdAt}
+              title={entry.title}
+              excerpt={entry.body} />
           ))}
+        </Container>
+        <Container marginBottom="default">
+          <ButtonLink dest="/news/" right>Alle News anzeigen</ButtonLink>
         </Container>
       </>
     )
